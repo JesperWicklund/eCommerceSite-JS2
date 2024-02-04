2a0ee665de55fd3e7365a8a309ab043556f49f83
Jesper <jesper.wicklund@outlook.com>
2024-02-04 13:29:48 +0100

    added hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
 
 export const Hero = () => {
   return (
-    <div className='flex justify-center items-center h-96 bg-blue-500'>
-        <p className=''>Hero Section</p>
+    <div className="flex justify-center items-center min-h-[350px] ">
+      <div className="border rounded-md p-8 w-3/4 flex flex-col gap-6 items-center">
+        <h1 className="tracking-wider text-2xl font-">
+          Välkommen till GadgetGalleriet
+        </h1>
+        <div className="w-3/4">
+          <p>
+            Välkommen till GadgetGalleriet - din ultimata destination för
+            innovativa elektroniska produkter! Hos oss öppnar du dörren till en
+            värld av teknologiska underverk och smarta lösningar.
+            GadgetGalleriet är mer än bara en elektronikbutik; det är en plats
+            där nysgieriga själar och teknikentusiaster samlas för att utforska
+            och upptäcka det senaste inom elektronikvärlden.
+          </p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import { ProductCard } from './ProductCard'
+import React from "react";
+import { ProductCard } from "./ProductCard";
 
 export const ProductList = ({ products }) => {
   return (
-    <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4  gap-4 p-4'>
-        { products.map(product => (
-            <ProductCard key={product._id} product={product}/>
+    <>
+    <h1 className="pl-4 text-3xl uppercase">Produkter</h1>
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4  gap-4 p-4">
+        {products.map((product) => (
+          <ProductCard key={product._id} product={product} />
         ))}
-    </div>
-  )
-}
+      </div>
+    </>
+  );
+};
